06c43edc2a4113ed48b8a17f6b6120455ce0e961
ales.potocnik@janushenderson.com <ales.potocnik@janushenderson.com>
2021-06-04 10:49:13 +0100

    Fix json equality compare

--- test-diff/Demo.Test.Integration/BigFileCompare.cs
+++ test-diff/Demo.Test.Integration/BigFileCompare.cs
@@ -1,4 +1,5 @@
 using FluentAssertions;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.IO;
 using Xunit;
@@ -7,24 +8,24 @@ namespace Demo.Test.Integration
 {
     public class BigFileCompare
     {
-        private const string FileAPath = "stub/fileA.json";
-        private const string FileBPath = "stub/fileB.json";
-        private const string FileCPath = "stub/fileC.json";
+        private const string FileAPath = "stub/bigFileA.json";
+        private const string FileBPath = "stub/bigFileB.json";
+        private const string FileCPath = "stub/bigFileC.json";
 
         [Fact]
         public void WhenFilesAreEqual_ShowsNoChanges()
         {
-            var objectA = JObject.Parse(File.ReadAllText(FileAPath));
-            var objectB = JObject.Parse(File.ReadAllText(FileBPath));
-            objectB.Should().Equal(objectA);
+            var objectA = JsonConvert.DeserializeObject(File.ReadAllText(FileAPath));
+            var objectB = JsonConvert.DeserializeObject(File.ReadAllText(FileBPath));
+            objectB.Should().BeEquivalentTo(objectA);
         }
 
         [Fact]
         public void WhenFilesAreDifferent_ShowsChanges()
         {
-            var objectA = JObject.Parse(File.ReadAllText(FileAPath));
-            var objectC = JObject.Parse(File.ReadAllText(FileCPath));
-            objectC.Should().NotBeSameAs(objectA);
+            var objectA = JsonConvert.DeserializeObject(File.ReadAllText(FileAPath));
+            var objectC = JsonConvert.DeserializeObject(File.ReadAllText(FileCPath));
+            objectC.Should().NotBe(objectA);
         }
     }
 }
